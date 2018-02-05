378bbeb7febfec7f70c55cfe421003504493b3a3
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-02-05 18:20:25 -0200

    Criando um Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,26 +16,22 @@ import {InputMaskModule} from 'primeng/inputmask';
 import {CurrencyMaskModule} from 'ng2-currency-mask';
 
 import {AppComponent} from './app.component';
-import {LancamentosPesquisaComponent} from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+
 import {NavbarComponent} from './navbar/navbar.component';
 import {PessoasPesquisaComponent} from './pessoas-pesquisa/pessoas-pesquisa.component';
-import {LancamentoCadastroComponent} from './lancamento-cadastro/lancamento-cadastro.component';
-import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
-import { MessageComponent } from './message/message.component';
-import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
-import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
+import {PessoaCadastroComponent} from './pessoa-cadastro/pessoa-cadastro.component';
+import {MessageComponent} from './message/message.component';
+import {PessoasGridComponent} from './pessoas-grid/pessoas-grid.component';
+import {LancamentosModule} from './lancamentos/lancamentos.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LancamentosPesquisaComponent,
     NavbarComponent,
     PessoasPesquisaComponent,
-    LancamentoCadastroComponent,
     PessoaCadastroComponent,
     MessageComponent,
-    LancamentosGridComponent,
     PessoasGridComponent
   ],
   imports: [
@@ -51,7 +47,8 @@ import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
     SelectButtonModule,
     DropdownModule,
     CurrencyMaskModule,
-    InputMaskModule
+    InputMaskModule,
+    LancamentosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
