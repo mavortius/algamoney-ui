da7a12ca5aadb5c438fcedef469dfb5c47f0ed02
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-02-15 09:40:00 -0200

    Criando classes de modelo e usando no cadastro de lançamentos

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -3,6 +3,8 @@ import {Component, OnInit} from '@angular/core';
 import {CategoriaService} from '../../categorias/categoria.service';
 import {ErrorHandlerService} from '../../core/error-handler.service';
 import {PessoaService} from '../../pessoas/pessoa.service';
+import {Lancamento} from "../../core/model";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-lancamento-cadastro',
@@ -16,8 +18,8 @@ export class LancamentoCadastroComponent implements OnInit {
   ];
 
   categorias = [];
-
   pessoas = [];
+  lancamento = new Lancamento();
 
   constructor(private categoriaService: CategoriaService,
               private pessoaService: PessoaService,
@@ -27,6 +29,10 @@ export class LancamentoCadastroComponent implements OnInit {
   ngOnInit() {
     this.carregarCategorias();
     this.carregarPessoas();
+  }
+
+  salvar(form: FormControl) {
+    console.log(this.lancamento);
   }
 
   carregarCategorias() {
