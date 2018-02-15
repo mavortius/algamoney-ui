184b7bdbdfc4b6f5811da9b9b8fd7d75a6f346a5
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-02-15 14:23:12 -0200

    Configurando rotas na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {LOCALE_ID, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
-
-import {ConfirmationService} from 'primeng/api';
+import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {LancamentosModule} from './lancamentos/lancamentos.module';
 import {PessoasModule} from './pessoas/pessoas.module';
 import {CoreModule} from './core/core.module';
-import {LancamentoService} from './lancamentos/lancamento.service';
-import {PessoaService} from './pessoas/pessoa.service';
+import {LancamentosPesquisaComponent} from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import {LancamentoCadastroComponent} from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import {PessoasPesquisaComponent} from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
 
+const routes: Routes = [
+  {path: 'lancamentos', component: LancamentosPesquisaComponent},
+  {path: 'lancamentos/novo', component: LancamentoCadastroComponent},
+  {path: 'pessoas', component: PessoasPesquisaComponent}
+];
 
 @NgModule({
   declarations: [
@@ -21,6 +26,7 @@ import {PessoaService} from './pessoas/pessoa.service';
     BrowserModule,
     BrowserAnimationsModule,
     HttpModule,
+    RouterModule.forRoot(routes),
 
     CoreModule,
     LancamentosModule,
