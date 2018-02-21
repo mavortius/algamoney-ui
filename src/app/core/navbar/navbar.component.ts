3a41d73082db3d22db965f15a9781a0c055d43f7
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-02-21 11:18:24 -0300

    Exibindo o nome do usuário logado

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 
+import {AuthService} from '../../seguranca/auth.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,4 +9,7 @@ import {Component} from '@angular/core';
 })
 export class NavbarComponent {
 
+
+  constructor(private auth: AuthService) {
+  }
 }
