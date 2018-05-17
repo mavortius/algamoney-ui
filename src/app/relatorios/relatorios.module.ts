382a6a1886030df9ea6ad65de99a64cd893259e0
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-05-17 16:46:34 -0300

    Configurando formulário do relatório

--- src/app/relatorios/relatorios.module.ts
+++ src/app/relatorios/relatorios.module.ts
@@ -1,16 +1,22 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+import {CalendarModule} from 'primeng/primeng';
 
 import {RelatoriosRoutingModule} from './relatorios-routing.module';
 import {SharedModule} from '../shared/shared.module';
-import { RelatorioLancamentosComponent } from './relatorio-lancamentos/relatorio-lancamentos.component';
+import {RelatorioLancamentosComponent} from './relatorio-lancamentos/relatorio-lancamentos.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    CalendarModule,
     SharedModule,
     RelatoriosRoutingModule
   ],
   declarations: [RelatorioLancamentosComponent]
 })
-export class RelatoriosModule { }
+export class RelatoriosModule {
+}
