718f286671240cda8dfe5945bc3341ef84f608aa
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-07-10 17:47:58 -0300

    Rodando comando ng build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://awmoney-api.herokuapp.com',
-  tokenWhitelistedDomains: [ /awmoney-api.herokuapp.com/ ],
-  tokenBlacklistedRoutes: [/\/oauth\/token/]
+  tokenWhitelistedDomains: [new RegExp('awmoney-api.herokuapp.com')],
+  tokenBlacklistedRoutes: [new RegExp('\/oauth\/token')]
 };
